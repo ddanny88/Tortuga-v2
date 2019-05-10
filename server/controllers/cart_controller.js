1b314b5126532dad5ddaefeda6bec5a070b06828
Daniel Dominguez <ddominguez20.dd@gmail.com>
2019-05-10 16:05:22 -0500

    minor

---
+++ server/controllers/cart_controller.js
@@ -0,0 +1,4 @@
+/**
+ *  addToCart: when an item is added to the cart, the item object is pushed into the req.session.cart and displayed in the cart. 
+ * additional functionality to calulate the price. Use composition. 
+*/

fdc8d78cdd80d81d412f886325cb0d0923afa9ac
Daniel Dominguez <ddominguez20.dd@gmail.com>
2019-05-22 10:55:31 -0500

    fixed details loading page

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -2,39 +2,49 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import './ItemDetail.css';
+import Loading from '../Loading/Loading';
+
 
 
 const ItemDetails = (props) => {
     const { drink } = props;
 
+    console.log(props)
     return (
         <div>
-            <Link to="/"><i className="fas fa-chevron-left"></i></Link>
-            <div className="ItemDetails-card">
-            <div>
-                <img 
-                    className="card-img"
-                    src={drink.img} 
-                    alt={drink.name}
-                />
-            </div>
-            <h1 className="card-heading">{drink.name}</h1>  
-            <p className="item-price">${drink.price}</p>
-            <p className="item-size">{drink.size}</p>
-            <div className="details-disc">
-                <p>{drink.discription}</p>
-            </div>
-            <div className="item-button-container">
-                <Button 
-                    text="Add to cart"
-                    textColor="white"
-                    color="#781214"
-                    fontSize="20px"
-                />
-            </div>
-        </div>
+            {
+                props.drink ?
+                
+                <div>
+                    <Link to="/"><i className="fas fa-chevron-left"></i></Link>
+                    <div className="ItemDetails-card">
+                    <div>
+                        <img 
+                            className="card-img"
+                            src={drink.img} 
+                            alt={drink.name}
+                        />
+                    </div>
+                    <h1 className="card-heading">{drink.name}</h1>  
+                    <p className="item-price">${drink.price}</p>
+                    <p className="item-size">{drink.size}</p>
+                    <div className="details-disc">
+                        <p>{drink.discription}</p>
+                    </div>
+                    <div className="item-button-container">
+                        <Button 
+                            text="Add to cart"
+                            textColor="white"
+                            color="#781214"
+                            fontSize="20px"
+                        />
+                    </div>
+                </div>
+                </div>
+                :
+                <Loading/>
+            }
         </div>
     )
 }
 export default ItemDetails;
-
